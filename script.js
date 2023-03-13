a13f123ec24b34154cd8ffe182af7cc916f728d9
trble91 <daleyaaron@gmail.com>
2023-03-13 09:56:49 -0400

    updates cards for war

---
+++ script.js
@@ -0,0 +1,28 @@
+class Card {
+    constructor(suits,ranks,value) {
+        this.suits = suits
+        this.ranks = ranks 
+        this.val = value 
+    }
+}
+
+class Deck {
+    constructor() {
+        this.cards = []
+    }
+
+    createDeck() {
+        let suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds']
+        let ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+
+        for(let i = 0; i < suits.length; i++) {
+            for(let j = 0; j < ranks.length; j++) {
+                this.cards.push(new Card(suits[i], ranks[j], j + 2))
+            }
+        }
+    }
+}
+
+
+const deck = new Deck()
+console.log(deck)
