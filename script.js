9790be4f6de7f01233d6b6224cb56ce29fb8b228
trble91 <daleyaaron@gmail.com>
2023-03-14 10:05:34 -0400

    updates war

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Create a card/deck class and shuffle the deck
 class Card {
     constructor(suits,ranks,value) {
         this.suits = suits
@@ -8,7 +9,7 @@ class Card {
 
 class Deck {
     constructor() {
-        this.cards = []
+        this.cards = [];
         this.createDeck()
     }
 
@@ -29,7 +30,76 @@ class Deck {
         this.cards = this.cards.sort((a,b) => 0.5 - Math.random())
     }
 }
+class GameOfWar {
+    constructor(){
+        this.p1 = [];
+        this.p2 = [];
+        this.pile = [];
+        this.gameInit();
+    }
 
+// Split cards into two players
+// Create an empty to store the pile  
+// Set the length of the pile to 0 
+//Initialize the game and create
+
+    gameInit() {
+        let deck = new Deck()
+        this.p1 = deck.cards.splice(0,26) //deck.cards.splice(0, deck.cards.length / 2)// this.p1.push(...deck.cards.splice(0, deck.cards.length / 2))
+        this.p2 = deck.cards.splice(27,52) //deck.cards // this.p2.push(...deck.cards.splice)
+    }
+     
+    startGame() {
+        // Set up game logic loop (a player has no more cards)
+        let p1card = this.p1.pop();
+        let p2card = this.p2.pop();
+
+        if  (p1card.val === p2card.val){
+            console.log("War!!!");
+            this.pile.push(p1card, p2card)
+            this.war()
+        } else if (p1card.val > p2card.val) {
+        this.p1.unshift(p2card, p1card, ...this.pile)
+        //clear the pile
+        console.log("Player 1 Wins!", this.p1.length); 
+    } else {
+        this.p2.unshift(p1card, p2card, ...this.pile)
+        //clear the pile
+        console.log("Player 2 Wins!", this.p2.length);
+        }
+    }
+    war() {
+    // Adjust for edge cases (a player does not have enough cards)
+     this.pile.push(...this.p1.splice(0, 3))
+     this.pile.push(...this.p2.splice(0, 3))
+
+    } 
+}
+let game = new GameOfWar();
+
+// //The initial battle between the two players 
+
+    battle() {
+        this.p1card = this.p1.pop();
+           
+        this.p2card = this.p2.pop()
+        if (p1card.value > p2card.value) {
+            
+        }
+        else if (p2card.value > p1card.value) {
+            
+        }
+        else war()
+        }
+//
+    war() {
+        this.p1.unshift(p2card,p1card)     
+    }
+};
+// clear pile this pile = 0
 
 const deck = new Deck()
 console.log(deck)
+console.log(this.p1.length)
+
+
